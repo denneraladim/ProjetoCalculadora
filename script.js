1bf633de57411307112db935c710c73164622c5a
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2023-08-28 15:40:54 -0300

    Projeto atualizado e concluido

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-document.querySelector('div')
+// Selecionar os Itens
+const resultado = document.querySelector('.result');
+const confirmar = document.querySelector('.igual');
+
+// Funções
+function insert(valor) {
+    resultado.innerHTML += valor;
+}
+
+function clean() {
+    resultado.innerHTML = '';
+}
+
+function backspace() {
+    if(resultado.textContent) {
+        let result = document.querySelector('#resultado').innerHTML
+        resultado.innerHTML = result.substring(0, result.length -1)
+    }
+}
+
+function confirma() {
+    if(resultado.textContent !== 'Erro') {
+        document.querySelector('#resultado').innerHTML = eval(resultado.innerHTML)
+    }
+}
